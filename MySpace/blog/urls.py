15e0fc559c1c80573c6a8b2bc0fd1eef4e3926ec
cojocarubianca <bitzu_c@yahool.com>
2016-01-16 15:01:50 +0200

    Post Detail View added

--- MySpace/blog/urls.py
+++ MySpace/blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^login/$', views.login_view, name='login'),
     url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^register/$', views.RegisterCreateView.as_view(), name='register'),
+    url(r'^post/(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post_details'),
+
 ]
--- MySpace/blog/views.py
+++ MySpace/blog/views.py
@@ -1,16 +1,18 @@
 from django.contrib.auth import authenticate, login, logout
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.shortcuts import render, redirect
 
+
 from blog.forms import UserLoginForm, RegisterForm
-from blog.models import UserInfo
+from blog.models import UserInfo, Post, Likes
+
 
 def login_view(request):
     if request.method == 'GET':
         form = UserLoginForm()
         context = {'form': form}
         return render(request, 'login.html', context)
-    elif request.method == 'POST':
+    elif request.me1thod == 'POST':
         form = UserLoginForm(request.POST)
         username = request.POST['username']
         password = request.POST['password']
@@ -36,5 +38,40 @@ class RegisterCreateView(CreateView):
     form_class = RegisterForm
     template_name = 'register.html'
 #    success_url = ''
-            
 
+
+class PostDetailView(DetailView):
+
+    model = Post
+    template_name = 'post_details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        post = self.get_object()
+        context1 = Likes.objects.filter(post = post.pk)
+       # context2 = Likes.objects.author(post = post.pk)
+        context['likes_list'] = context1
+        context['likes_list_top3'] = context1[:3]
+        context['likes_count'] = len(context1)     
+        return context
+
+
+
+'''def post_details(request, pk):
+    post = UserPost.objects.get(pk=pk)
+    if request.method == 'GET':
+        form = UserPostCommentForm()
+        context = {
+            'post': post,
+            'form': form,
+        }
+        return render(request, 'post_details.html', context)
+    elif request.method == 'POST':
+        form = UserPostCommentForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+            comment = UserPostComment(text=text, post=post, author=request.user)
+            comment.save()
+        return redirect('post_details', pk=pk)'''
+
+ 
