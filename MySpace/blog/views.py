8051a6aa17aba3acb1929042d57be0c2c7316354
Tudor67 <buzu.tudor67@gmail.com>
2016-01-16 12:35:47 +0200

    urls added in /blog

--- MySpace/blog/views.py
+++ MySpace/blog/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
+from blog.forms import UserLoginForm
+from blog.models import UserInfo
+
+def login_view(request):
+    if request.method == 'GET':
+        form = UserLoginForm()
+        context = {'form': form}
+        return render(request, 'login.html', context)
+    elif request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is None:
+            context = {
+                'form': form,
+                'message': 'Wrong username or password!'
+            }
+            return render(request, 'login.html', context)
+
+        else:
+            login(request, user)
+            return redirect('index')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
