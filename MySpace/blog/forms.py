f5d00cdb61319908f4389f7f2d95d910d50d1b80
BirsanDaniela <birsan.daniela17@gmail.com>
2016-01-16 14:09:34 +0200

    Register Page

--- MySpace/blog/forms.py
+++ MySpace/blog/forms.py
@@ -1,5 +1,29 @@
 from django.forms import Form, CharField, DateTimeField, ImageField, Textarea, PasswordInput
+from django.forms import ModelForm
+from blog.models import UserInfo
+from django.contrib.auth.forms import UserCreationForm
+
 
 class UserLoginForm(Form):
     username = CharField(max_length=30)
     password = CharField(widget=PasswordInput)
+
+
+class RegisterForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.user = UserCreationForm()
+        self.fields.update(self.user.fields)
+
+    
+    def save(self, commit=True):
+        user = self.user.save()
+        self.instance.user = user
+        return super(RegisterForm, self).save(commit=commit)
+
+    class Meta:
+        model = UserInfo
+        fields = ['first_name', 'last_name', 'birth_date', 'email']
+
+
--- MySpace/blog/urls.py
+++ MySpace/blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 urlpatterns = [
     url(r'^login/$', views.login_view, name='login'),
     url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^register/$', views.RegisterCreateView.as_view(), name='register'),
 ]
