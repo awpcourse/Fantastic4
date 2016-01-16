d3e9060aff30c979da4fe087828b7f6862965717
marianaPatriche <mariana.patriche23@gmail.com>
2016-01-16 15:13:20 +0200

    index page

--- MySpace/blog/urls.py
+++ MySpace/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^login/$', views.login_view, name='login'),
     url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^register/$', views.RegisterCreateView.as_view(), name='register'),
+    url(r'^$', views.PostListView.as_view(), name='index'),
 ]
